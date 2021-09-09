30c37f35c5f85647e10a59693291f62b6c49d328
Vitaly Murzin <adn4ops@gmail.com>
2021-09-09 14:29:52 +0500

    add test_db.sql

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop postgres_srv
 docker rm postgres_srv
-docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=pass -e POSTGRES_USER=user -e POSTGRES_DB=netology -v /DATABASE/postgres-data:/var/lib/postgreql/data -v /DATABASE/postgres-backup:/mnt --name postgres_srv postgres
+docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=pass -e POSTGRES_USER=user -e POSTGRES_DB=netology -v /DATABASE/postgres-data:/var/lib/postgresql/data -v /DATABASE/postgres-backup:/mnt --name postgres_srv postgres
